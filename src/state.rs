f679c671611d2573625897834b396db6ca4b28fd
YangHgRi <yanghgri@gmail.com>
2025-08-05 20:58:42 +0800

    feat(state): 添加 Pool 和 Tick 结构体

--- src/state.rs
+++ src/state.rs
@@ -17,3 +17,26 @@ impl TokenType {
         amount as f64 / 10f64.powi(decimals)
     }
 }
+
+#[derive(Debug)]
+pub struct Pool {
+    pub token_0: TokenType,
+    pub token_1: TokenType,
+    pub fee: u32,
+    pub tick_spacing: u16,
+    pub total_fee0: u128,
+    pub total_fee1: u128,
+    pub activity_liquidity: u128,
+    pub sqrt_price: u128,
+    pub current_tick: i32,
+    pub balance0: u128,
+    pub balance1: u128,
+}
+
+pub struct Tick {
+    pub tick: i32,
+    pub liquidity: u128,
+    pub liquidity_gross: u128,
+    pub fee_growth_outside_0: u128,
+    pub fee_growth_outside_1: u128,
+}
